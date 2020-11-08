674b14f51d146284a71c514893045539244d7271
SirHawbly <silversteinrulz@gmail.com>
2020-11-07 23:52:18 -0800

    added add/rem-piece functions, max list value, documentation, changed imports

--- src/piece_list.py
+++ src/piece_list.py
@@ -1,11 +1,15 @@
-from coord import coord_class 
-from piece import piece_class 
+# imports
+import coord
+import piece
+
 from piece_type import piece_type_list
 
+
+# class object
 class piece_list_class:
 
     # constructor
-    def __init__(self, piece_list=[], piece_color=-1):
+    def __init__(self, piece_list=[], piece_color=-1) -> None:
         """
         """
 
@@ -13,11 +17,14 @@ class piece_list_class:
         # all pieces inside are actually pieces.
         assert isinstance(piece_list, list)
 
-        for piece in piece_list:
-            assert(isinstance(piece, piece_class))
+        for piece_i in piece_list:
+            assert(isinstance(piece_i, piece.piece_class))
 
         # verify that the passed color is an int.
         assert isinstance(piece_color, int)
+
+        # set piece list max size
+        self.max_size = 10
 
         # set data members of class to passed values.
         self.pieces = piece_list
@@ -28,52 +35,109 @@ class piece_list_class:
 
     
     # check list for each item's type + color
-    def assert_member_types(self, ):
+    def assert_member_types(self, ) -> None:
         """
         """
         
-        for piece in self.pieces:
-            assert(isinstance(piece, piece_class))
-            assert(piece.color == self.list_color)
+        assert isinstance(self.pieces, list)
+
+        # loop through the pieces in the list, verify color and type
+        for piece_i in self.pieces:
+            assert(isinstance(piece_i, piece.piece_class))
+            assert(piece_i.color == self.list_color)
+        
+        assert self.max_size == 10
+
+        assert isinstance(self.list_color, int)
+
+
+    # add piece
+    def add_piece(self, new_piece) -> None:
+        """
+        """
+
+        self.assert_member_types()
+
+        if len(self.pieces) > self.max_size:
+            assert False
+        else:
+            self.pieces += [new_piece, ]
+    
+
+    # remove piece
+    def remove_piece(self, new_piece) -> None:
+        """
+        """
+
+        self.assert_member_types()
+
+        if len(self.pieces) > 0:
+            for piece_i in self.pieces:
+                if piece_i == new_piece:
+                    self.pieces.remove(new_piece)
+        else:
+            assert False
 
 
     # check for king
-    def has_king(self, ):
+    def has_king(self, ) -> bool:
         """
         """
 
-        for piece in self.pieces:
-            if piece.is_of_type(piece_type_list.king):
+        self.assert_member_types()
+
+        for piece_i in self.pieces:
+            if piece_i.is_of_type(piece_type_list.king):
                 return True
         
         return False
     
 
     # total points of pieces in list
-    def get_total_points(self, ):
+    def get_total_points(self, ) -> int:
         """
         """
+
+        self.assert_member_types()
 
         total = 0
         
-        for piece in self.pieces:
-            total += piece.weight
+        for piece_i in self.pieces:
+            total += piece_i.weight
         
         return total
 
+
     # string function
-    def toString(self, ):
+    def toString(self, ) -> str:
         """
         """
 
-        return ""
+        self.assert_member_types()
+
+        ret_str = '{} - < '.format(self.list_color) 
+
+        if len(self.pieces) == 0:
+            ret_str += ' '
+
+        else:
+            for piece_i in self.pieces:
+                ret_str += '{} '.format(piece_i.toString())
+
+        return ret_str + '>'
 
 
 # ---
 
-def main():
-    print("")
-        
+def main() -> None:
+    """
+    """
+
+    pl = piece_list_class()
+
+    pl.add_piece(piece_type_list.king)
+    
+    print(pl.toString())
 
 # ---
 
