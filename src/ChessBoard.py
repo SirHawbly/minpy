cf419241d9ef872e5c3dd54f7bbce315496dc8eb
Chris Bartlett <sirhawbly@github.com>
2023-02-16 17:01:46 -0800

    added chess board class

---
+++ src/ChessBoard.py
@@ -0,0 +1,45 @@
+from ChessPiece import ChessPiece
+
+board_header = " | a | b | c | d | e |\n"
+
+
+class ChessBoard:
+
+  def __init__(self, turn: int = 0) -> None:
+
+    self.board = []
+    self.turn = turn
+    self.turn_limit = 40 # 20 per player
+
+    for j in range(0, 6):
+      row = []
+
+      for i in range(0, 5):
+        row += [ChessPiece('.', j, i, 'n'), ]
+
+        # print(row)
+        self.board += [row, ]
+
+    # print(self.board)
+
+  def __str__(self) -> str:
+    ret_str = 'turn: ' + str(self.turn) + '\n' + board_header
+
+    for j in range(0, 6):
+
+      ret_str += str(j) + '| '
+
+      for i in range(0 ,5):
+        ret_str += self.board[j][i].piece_type + ' | '
+
+      ret_str += '\n'
+
+    return ret_str
+
+  def reset_board(self) -> None:
+    return
+
+
+if __name__ == "__main__":
+  test_chess_board = ChessBoard()
+  print(test_chess_board)
